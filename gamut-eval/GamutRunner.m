274b4eb84bc9a4bdd0e78dcf8dc3ddf665847e5f
andy <andyshenproper@gmail.com>
2021-08-16 16:33:47 -0700

    Fiddle with projections using matlab colorcloud

--- gamut-eval/GamutRunner.m
+++ gamut-eval/GamutRunner.m
@@ -1,6 +1,14 @@
 file = 'res/neutral.png';
 
 img = imread(file);
-gamut_volume(img);
+% gamut_volume(img);
+colorcloud(img);
+view([-1, -1, -1])
+camproj('perspective');
+view([-1, -1, -1])
+grid off
+box on
+pbaspect([1 1 1])
+set(gcf, 'Position', [100, 100, 512, 512])
 
 % generateCCGamuts(file);
